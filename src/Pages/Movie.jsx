2ac072e4cf3b5f6c18367e562241271d13fb7868
Julia <juliakeo@pm.me>
2025-12-18 15:18:27 +0100

    Changed name again

--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router'
 import styled from "styled-components"
-import { StarIcon, ArrowIcon } from '../Components/blabla'
+import { StarIcon, ArrowIcon } from '../Components/Icons'
 
 const apiKey = '1126aa859065d4eba48bcfccb414407a'
 
